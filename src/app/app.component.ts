9ff60d63c9f33d305011ed0c74e23125d59c9a3b
ozngnr <ozngnr@gmail.com>
2023-12-03 16:18:11 +0000

    added cards component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { CommonModule } from '@angular/common';
+import { CardsComponent } from './components/cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, HeroComponent],
+  imports: [CommonModule, HeaderComponent, HeroComponent, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
